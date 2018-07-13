0ed2b9e1d009737b29179bdae785add3090455f8
zhupeiqing <33336432+zhupeiqing@users.noreply.github.com>
2018-07-13 11:27:29 +0800

    Add files via upload

---
+++ digitalReadSerial.ino
@@ -0,0 +1,25 @@
+int ledpin = 9;
+int nowled = 0;
+void setup() {
+  pinMode(ledpin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+/*  int sensorValue = digitalRead(4);
+  if(sensorValue == 1){
+    if(nowled == 0){
+      digitalWrite(ledpin,HIGH);
+      nowled = 1; 
+    }
+    else{
+      digitalWrite(ledpin,LOW);
+      nowled = 0;
+    }
+   }
+ */
+ // Serial.println(nowled);
+analogWrite(ledpin,10);
+ 
+//  delay(1);  
+}
